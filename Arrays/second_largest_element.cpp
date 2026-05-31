5f6b42cd3f78b644959023743a9f91d03cd624be
Swapnila <swapnilapersonal@gmail.com>
2026-05-31 12:35:56 +0530

    Added second largest element array solution

---
+++ Arrays/second_largest_element.cpp
@@ -0,0 +1,24 @@
+//optimal approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int secondLargest(vector<int>&arr){
+int largest=arr[0];
+int slargest=-1;
+for(int i=1;i<arr.size();i++){
+    if(arr[i]>largest){
+        slargest=largest;
+        largest=arr[i];
+    }
+    else if(arr[i]>slargest && arr[i]!=largest){
+        slargest=arr[i];
+    }
+}
+return slargest;
+}
+
+int main(){
+vector<int>arr={4,2,3,7,7};
+cout<<secondLargest(arr)<<endl;
+return 0;
+}
