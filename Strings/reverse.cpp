52eced661f98cd934c0e328043c3a9ae652ff8a3
Swapnila <swapnilapersonal@gmail.com>
2026-06-04 18:54:24 +0530

    Reverse of string

---
+++ Strings/reverse.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+string str ="Hello";
+int left =0;
+int right=str.length()-1;
+while(left<right){
+    swap(str[left],str[right]);
+    left++;
+    right--;
+}
+cout<<str;
+return 0;
+}
